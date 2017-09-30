84c0be57ab0cc609f35d9dda4e8d34c3d390077a
Oliver Nural <on36@kent.ac.uk>
2017-09-30 16:40:41 +0100

    added google analytics and typo

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -17,7 +17,7 @@ class App extends Component {
 
                 <p>During my year I worked in Agile teams, developing Web apps in both Java and Javascript, versioned my work with Git and learnt a great deal of interpersonal skills along the way.</p>
 
-                <p>In my spare time I volunteer at a local Church coffee shop, and enjoy playing Saxophone and Piano. And when I'm not doing any of the above, i'm working on my own <a href="https://github.com/OllyNural" target="github"><span className="title-name">side projects</span></a> (including this website), or playing atmospheric and envoloping video games.</p>
+                <p>In my spare time I volunteer at a local Church coffee shop, and enjoy playing Saxophone and Piano. And when I'm not doing any of the above, I'm working on my own <a href="https://github.com/OllyNural" target="github"><span className="title-name">side projects</span></a> (including this website), or playing atmospheric and envoloping video games.</p>
               </div>
             </div>
           </div>
