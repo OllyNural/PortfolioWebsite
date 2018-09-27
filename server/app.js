cc6a4195f3a6806478cff7c417ff9cfac82d0e86
Oliver Nural <onural@and.digital>
2018-09-27 17:14:48 +0100

    Removed data and ability to send an email

--- server/app.js
+++ server/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const data = require('./data.js');
 
 const nodemailer = require('nodemailer');
 
@@ -27,29 +26,6 @@ app.use(express.static(path.resolve(__dirname, '..', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'))
 });
-
-// POST Request to send an email
-app.post('/api/contact', (req, res) => {
-  let data = req.body;
-  
-  // Init Mail Options to send
-  let mailOptions = {
-    from: 'oliver.nural@gmail.com',
-    to: 'oliver.nural@gmail.com',
-    subject: 'hello subject',
-    text: 'hello text'
-  };
-  
-  transport.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return error;
-    } else {
-      return true;
-    }
-  });
-  
-})
-
 
 
 module.exports = app;
--- server/data-example.js
+++
@@ -1,6 +0,0 @@
-const Data = {
-  user: 'some@email.com',
-  password: 'SomeP4ssw0rd'
-}
-
-module.exports = Data;
